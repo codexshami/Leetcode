3ac41dfefb6018a8baace8ae2803058021ae10e3
Mohd Shami <shamisheikh1786@gmail.com>
2026-05-17 20:05:02 +0530

    Implement DFS solution for jump game III
    Added a DFS solution to check if zero can be reached.

--- 1306_jump_game_III.py
+++ 1306_jump_game_III.py
@@ -58,3 +58,23 @@ class Solution:
                     q.append(nxt)
 
         return False
+#DSF solution
+class Solution:
+    def canReach(self, arr, start):
+        n = len(arr)
+        visited = [False] * n
+
+        def dfs(i):
+            # Out of bounds or already visited
+            if i < 0 or i >= n or visited[i]:
+                return False
+
+            # Found zero
+            if arr[i] == 0:
+                return True
+
+            visited[i] = True
+
+            return dfs(i + arr[i]) or dfs(i - arr[i])
+
+        return dfs(start)
